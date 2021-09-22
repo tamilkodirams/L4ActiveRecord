4123a8426a73646650235bb1da7d083afd5f54e1
tamil <hod.mca@giet.ac.in>
2021-09-22 13:21:28 +0530

    Todo Management CLI app

--- todo.rb
+++ todo.rb
@@ -4,26 +4,30 @@ class Todo < ActiveRecord::Base
   def due_today?
     due_date == Date.today
   end
-
+  def self.overdue
+    where("due_date < ?", Date.today)
+  end
+  def self.due_today
+    where("due_date = ?", Date.today)
+  end
+  def self.duelater
+    where("due_date > ?", Date.today)
+  end
   def to_displayable_string
     display_status = completed ? "[X]" : "[ ]"
     display_date = due_today? ? nil : due_date
     puts "#{id} #{display_status} #{todo_text} #{display_date}"
   end
-
   def self.show_list
     puts "My Todo-List\n\n"
     puts "Overdue\n"
-    record = Todo.where("DATE(due_date) < DATE(?)", Date.today)
-    record.map { |todo| todo.to_displayable_string }
+    puts overdue.map { |todo| todo.to_displayable_string }
     puts "\n\n"
     puts "Due Today\n"
-    record = Todo.where(due_date: Date.today)
-    record.map { |todo| todo.to_displayable_string }
+    puts due_today.map { |todo| todo.to_displayable_string }
     puts "\n\n"
     puts "Due Later\n"
-    record = Todo.where("DATE(due_date) > DATE(?)", Date.today)
-    record.map { |todo| todo.to_displayable_string }
+    puts duelater.map { |todo| todo.to_displayable_string }
   end
   def self.add_task(h)
     puts "In add_task"
@@ -32,7 +36,9 @@ class Todo < ActiveRecord::Base
     Todo.create!(todo_text: h[:todo_text], due_date: Date.today + h[:due_in_days], completed: false)
   end
   def self.mark_as_complete(todo_id)
-    record = Todo.find_by(id: todo_id)
-    record.update(completed: true)
+    todo = Todo.find(todo_id)
+    todo.completed = true
+    todo.save
+    todo
   end
 end
